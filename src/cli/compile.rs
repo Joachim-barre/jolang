a2108c5d441dc3f3e233a7e782ea0b9870e4a354
Joachim-barre <joachim.barre22@laposte.net>
2024-07-16 20:01:06 +0200

    added file argument for the compile command

--- src/cli/compile.rs
+++ src/cli/compile.rs
@@ -1,6 +1,9 @@
 use clap::Args;
+use clio::{InputPath, ClioPath};
 
 #[derive(Args)]
 pub struct CompileArgs {
-
+    /// path to the file to compile
+    #[clap(value_parser = clap::value_parser!(ClioPath).exists().is_file())]
+    pub file : ClioPath
 }

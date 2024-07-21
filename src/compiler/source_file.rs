10603c3c482ae044dfa71c745d6c5e2d061f9bcf
Joachim-barre <joachim.barre22@laposte.net>
2024-07-21 16:25:35 +0200

    added header position to source file

--- src/compiler/source_file.rs
+++ src/compiler/source_file.rs
@@ -1,13 +1,22 @@
 use std::fs::File;
+use std::option::Option;
 
 pub struct SourceFile {
-    pub file : File
+    pub file : File,
+    pub text_start : Option<u64>,
+    pub text_end : Option<u64>,
+    pub data_start : Option<u64>,
+    pub data_end : Option<u64>
 }
 
 impl From<File> for SourceFile {
     fn from(file: File) -> Self {
         SourceFile {
-            file
+            file,
+            text_start : None,
+            text_end : None,
+            data_start : None,
+            data_end : None
         }
     }
 }

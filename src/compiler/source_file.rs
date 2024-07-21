f3796417e7f8cefc1e37988ed0c5ff68d48617c7
Joachim-barre <joachim.barre22@laposte.net>
2024-07-21 17:38:01 +0200

    added comments

--- src/compiler/source_file.rs
+++ src/compiler/source_file.rs
@@ -3,9 +3,13 @@ use std::option::Option;
 
 pub struct SourceFile {
     pub file : File,
+    // first text line
     pub text_start : Option<u64>,
+    // last text line
     pub text_end : Option<u64>,
+    // first data line
     pub data_start : Option<u64>,
+    // last data line
     pub data_end : Option<u64>
 }
 

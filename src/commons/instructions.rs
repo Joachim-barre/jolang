926d7d323d7e946cac0529c081a21eaf630f62cb
Joachim-barre <joachim.barre22@laposte.net>
2024-07-26 18:21:00 +0200

    made instruction a c_enum

--- src/commons/instructions.rs
+++ src/commons/instructions.rs
@@ -1,34 +1,37 @@
-#[derive(Debug,Clone, PartialEq, Eq)]
-#[repr(u8)]
-pub enum Instructions {
-    /// <
-    Backward = 0,
-    /// >
-    Forward = 1,
-    /// L
-    Load = 2,
-    /// S
-    Store = 3,
-    /// +
-    Add = 4,
-    /// -
-    Sub = 5,
-    /// *
-    Mul = 6,
-    /// /
-    Div = 7,
-    /// P
-    Print = 8,
-    /// ],
-    Jump = 10,
-    /// }
-    JumpIfZero = 11,
-    /// E
-    Exit = 12,
-    /// I
-    Inc = 13,
-    /// D
-    Dec = 14,
-    /// C
-    Compare = 15
+use c_enum::c_enum;
+
+c_enum! {
+    #[derive(Clone, PartialEq, Eq)]
+    pub enum Instructions : u8{
+        /// <
+        Backward = 0,
+        /// >
+        Forward = 1,
+        /// L
+        Load = 2,
+        /// S
+        Store = 3,
+        /// +
+        Add = 4,
+        /// -
+        Sub = 5,
+        /// *
+        Mul = 6,
+        /// /
+        Div = 7,
+        /// P
+        Print = 8,
+        /// ],
+        Jump = 10,
+        /// }
+        JumpIfZero = 11,
+        /// E
+        Exit = 12,
+        /// I
+        Inc = 13,
+        /// D
+        Dec = 14,
+        /// C
+        Compare = 15
+    }
 }

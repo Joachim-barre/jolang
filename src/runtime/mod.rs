da88059073a49ade762099ad54fa1fd194c8ca7b
Joachim-barre <joachim.barre22@laposte.net>
2024-07-29 17:03:39 +0200

    added file opening to the runtime

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,6 +1,19 @@
 use crate::cli::run::RunArgs;
-use std::result::Result;
+use std::{result::Result, fs::{File, OpenOptions}};
 
 pub fn run(args : RunArgs) -> Result<(), String> {
+    if !args.file.is_local() {
+        return Err(String::from("please input a local file"))
+    }
+    let file : File;
+    match OpenOptions::new().read(true).write(false).truncate(false).append(false).open(args.file.clone().as_os_str()) {
+        Ok(f) => {
+            file = f
+        }
+        Err(_) => {
+            return Err(String::from("can't open file"))
+        }
+    }
+    println!("loading object {}", args.file);
     todo!();
 }

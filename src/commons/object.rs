d128d6352e8ee38d9c80025f798d6091c73f1967
Joachim-barre <joachim.barre22@laposte.net>
2024-07-29 19:10:50 +0200

    added object load function

--- src/commons/object.rs
+++ src/commons/object.rs
@@ -1,6 +1,6 @@
 use crate::compiler::text_data::TextData;
 use super::instructions::Instructions;
-use std::{fs::File, io::Write};
+use std::{fs::File, io::{Read, Write}};
 
 #[derive(Debug)]
 pub struct Object {
@@ -41,5 +41,61 @@ impl Object {
             return Err(String::from("failed to write the compiled object"));
         }
         Ok(())
+    }
+
+    pub fn load(file : &mut File) -> Result<Self, String> {
+        let mut bytes :  Vec<u8> = Vec::new();
+
+        match file.read_to_end(&mut bytes) {
+            Ok(_) => {},
+            Err(_) => {
+                return Err("can't read file".to_string());
+            }
+        };
+
+        if bytes.len() < 36 {
+            return Err("object file too small".to_string());
+        }
+
+        if bytes[..4] != "\0JOO".as_bytes()[..4] {
+            return Err("bad header (unknown file format)".to_string())
+        }
+
+        let main_jump = u64::from_le_bytes(bytes[4..12].try_into().unwrap());
+        let jump_len = u64::from_le_bytes(bytes[12..20].try_into().unwrap()); 
+        if bytes.len() < (36 + jump_len*8) as usize {
+            return Err("bad jump table size".to_string())
+        }
+        let mut jumps = bytes.split_off(20);
+        bytes = jumps.split_off((jump_len*8) as usize);
+        let jumps : Vec<u64> = jumps.chunks_exact(8)
+            .map(|x| u64::from_le_bytes(x.try_into().unwrap()))
+            .collect();
+        let data_len = u64::from_le_bytes(bytes[..8].try_into().unwrap());
+        if bytes.len() < (16 + data_len*8) as usize {
+            return Err("bad data section size".to_string())
+        } 
+        let mut data = bytes.split_off(8);
+        bytes = data.split_off((data_len*8) as usize);
+        let data : Vec<i64> = data.chunks_exact(8)
+            .map(|x| i64::from_le_bytes(x.try_into().unwrap()))
+            .collect();
+        let text_len = u64::from_le_bytes(bytes[..8].try_into().unwrap());
+        if bytes.len() != (8+text_len) as usize {
+            return Err(String::from("bad text section size"))
+        }
+        let text : Vec<Instructions> = bytes.iter()
+            .skip(8)
+            .map(|x| Instructions::from(*x))
+            .collect();
+
+        let object = Self {
+            main_jump,
+            jumps,
+            data,
+            text
+        };
+        
+        Ok(object)
     }
 }
--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,11 +1,11 @@
-use crate::cli::run::RunArgs;
+use crate::{cli::run::RunArgs, commons::object::Object};
 use std::{result::Result, fs::{File, OpenOptions}};
 
 pub fn run(args : RunArgs) -> Result<(), String> {
     if !args.file.is_local() {
         return Err(String::from("please input a local file"))
     }
-    let file : File;
+    let mut file : File;
     match OpenOptions::new().read(true).write(false).truncate(false).append(false).open(args.file.clone().as_os_str()) {
         Ok(f) => {
             file = f
@@ -15,5 +15,6 @@ pub fn run(args : RunArgs) -> Result<(), String> {
         }
     }
     println!("loading object {}", args.file);
+    dbg!(Object::load(&mut file)?);
     todo!();
 }

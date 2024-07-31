7d85617d4a82bd36fe3db132017d7b4c6b9ca122
Joachim-barre <joachim.barre22@laposte.net>
2024-07-31 21:02:07 +0200

    removed pest

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -6,7 +6,6 @@ use crate::cli::compile::CompileArgs;
 use crate::commons::object::Object;
 pub mod source_file;
 pub mod text_data;
-pub mod parser;
 use text_data::TextData;
 use source_file::SourceFile;
 

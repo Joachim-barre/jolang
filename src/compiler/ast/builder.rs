41e2aa90a79d34fa22c8323e4678f3c5406da043
Joachim-barre <joachim.barre22@laposte.net>
2024-08-05 19:57:22 +0200

    implemented from for AstBuilder

--- src/compiler/ast/builder.rs
+++ src/compiler/ast/builder.rs
@@ -1,5 +1,13 @@
-use crate::compiler::lexer::LexerTokens;
+use crate::compiler::lexer::{Lexer, LexerTokens};
 
 pub struct AstBuilder<'a> {
-    token : LexerTokens<'a>
+    tokens : LexerTokens<'a>
+}
+
+impl<'a> From<&'a mut Lexer> for AstBuilder<'a> {
+    fn from(value: &'a mut Lexer) -> Self {
+        Self {
+            tokens : value.tokens()
+        }
+    }
 }

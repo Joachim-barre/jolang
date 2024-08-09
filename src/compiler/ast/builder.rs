5a56c4a5048857b95497db751d93fb5076f2e809
Joachim-barre <joachim.barre22@laposte.net>
2024-08-09 22:54:55 +0200

    added access to source in ast builder

--- src/compiler/ast/builder.rs
+++ src/compiler/ast/builder.rs
@@ -1,13 +1,19 @@
-use crate::compiler::lexer::{Lexer, LexerTokens};
+use crate::compiler::{compiler_error::{CompilerError, CompilerErrorKind},lexer::{Lexer, LexerTokens}, source_buffer::SourceBuffer};
+use super::{Program, Statement};
+use std::{cell::RefCell, rc::Rc};
 
 pub struct AstBuilder<'a> {
-    tokens : std::iter::Peekable<LexerTokens<'a>>
+    tokens : std::iter::Peekable<LexerTokens<'a>>,
+    source : Rc<RefCell<SourceBuffer>>
 }
 
 impl<'a> From<&'a mut Lexer> for AstBuilder<'a> {
     fn from(value: &'a mut Lexer) -> Self {
+        let source = value.source.clone();
         Self {
             tokens : value.tokens().peekable()
+            tokens : value.tokens().peekable(),
+            source 
         }
     }
 }

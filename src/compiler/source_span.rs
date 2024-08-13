ac93305cc250e4ffd7b2a9b2236338eab191a7f6
Joachim-barre <joachim.barre22@laposte.net>
2024-08-13 14:29:17 +0200

    modified source span

--- src/compiler/source_span.rs
+++ src/compiler/source_span.rs
@@ -1,19 +1,19 @@
-use super::source_buffer::{SourceBuffer, SourcePos};
+use super::{source_buffer::SourceBuffer, source_reader::SourceCursor};
 
 #[derive(Clone)]
 pub struct SourceSpan<'a> {
-    pub start : SourcePos,
-    pub end : SourcePos,
+    pub start : SourceCursor<'a>,
+    pub size : usize,
     pub data : &'a str,
     pub source : &'a SourceBuffer
 }
 
 impl SourceSpan<'_> {
-    pub fn at<'a> (source : &'a SourceBuffer, start : SourcePos, end : SourcePos) -> SourceSpan<'a> {
+    pub fn at<'a> (source : &'a SourceBuffer, start : SourceCursor<'a>, size : usize) -> SourceSpan<'a> {
         SourceSpan {
             start,
-            end,
-            data : &source.buffer[start.index..end.index],
+            size,
+            data : start.data_ref.split_at(size).0,
             source
         }
     }

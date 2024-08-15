813ca8f50ec76f13a862af6f6b1a00719dbfa851
Joachim-barre <joachim.barre22@laposte.net>
2024-08-15 17:44:37 +0200

    fixed (expr) overflowing the stack

--- src/compiler/ast/builder.rs
+++ src/compiler/ast/builder.rs
@@ -288,6 +288,7 @@ impl<'a> AstBuilder<'a> {
                 }?
             )),
             TokenKind::LParan => {
+                    self.next_token()?;
                     let sub_expr = self.parse_expr()?;
                     if !self.next_token()?.as_ref().map_or(false, |x| x.kind == TokenKind::RParan) {
                         return Err(self.expected("\")\""))

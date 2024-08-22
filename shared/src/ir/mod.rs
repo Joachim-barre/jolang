fb0d1c834463d7f2334e697009772636d6b10f9c
Joachim-barre <joachim.barre22@laposte.net>
2024-08-22 16:18:13 +0200

    made IrObject public

--- shared/src/ir/mod.rs
+++ shared/src/ir/mod.rs
@@ -1,7 +1,7 @@
 use instructions::Instruction;
 pub mod instructions;
 
-struct IrObject<'a> {
+pub struct IrObject<'a> {
     ext_fn : Vec<(String, u8, bool)>,
     variables : Vec<i64>,
     blocks : Vec<Vec<Instruction<'a>>>

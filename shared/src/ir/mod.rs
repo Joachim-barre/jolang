1c72647e36303332515763f8321fe85fd523a681
Joachim-barre <joachim.barre22@laposte.net>
2024-08-22 16:25:14 +0200

    added constructor for ir object

--- shared/src/ir/mod.rs
+++ shared/src/ir/mod.rs
@@ -8,3 +8,13 @@ pub struct IrObject<'a> {
     variables : Vec<i64>,
     blocks : Vec<Block<'a>>
 }
+
+impl<'a> IrObject<'a> {
+    pub fn new() -> Self {
+        Self{
+            ext_fn : Vec::new(),
+            variables : Vec::new(),
+            blocks : Vec::new()
+        }
+    }
+}

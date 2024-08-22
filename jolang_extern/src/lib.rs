127c4f65693931818337134f612a233daa32a95b
Joachim-barre <joachim.barre22@laposte.net>
2024-08-22 11:52:10 +0200

    Revert "created a proc macro crate for easier externs"
    This reverts commit 58c64fe08b8255d433455ec3b6895187e26151c0.

--- jolang_extern/src/lib.rs
+++ jolang_extern/src/lib.rs
@@ -1,9 +1,1 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_attribute]
-pub fn jolang_extern(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input(input as ItemFn);
-
-}

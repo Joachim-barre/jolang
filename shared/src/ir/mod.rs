7030a508bb91b2177c47d6c629b55b490b2364eb
Joachim-barre <joachim.barre22@laposte.net>
2024-08-22 16:09:18 +0200

    created Ir object struct

--- shared/src/ir/mod.rs
+++ shared/src/ir/mod.rs
@@ -1,1 +1,8 @@
+use instructions::Instruction;
 pub mod instructions;
+
+struct IrObject<'a> {
+    ext_fn : Vec<(String, u8, bool)>,
+    variables : Vec<i64>,
+    blocks : Vec<Vec<Instruction<'a>>>
+}

bdb1f33b49bc4df7e52f4f5584c3a400b91f028d
Joachim-barre <joachim.barre22@laposte.net>
2024-08-25 12:37:15 +0200

    added get_block methods

--- shared/src/ir/mod.rs
+++ shared/src/ir/mod.rs
@@ -2,11 +2,12 @@ use instructions::{operand::BlkId, Instruction};
 pub mod instructions;
 use block::Block;
 pub mod block;
+use std::cell::{Ref, RefCell, RefMut};
 
 pub struct IrObject<'a> {
     ext_fn : Vec<(String, u8, bool)>,
     variables : Vec<i64>,
-    blocks : Vec<Block<'a>>
+    blocks : Vec<RefCell<Block<'a>>>
 }
 
 impl<'a> IrObject<'a> {
@@ -19,7 +20,15 @@ impl<'a> IrObject<'a> {
     }
 
     pub fn append_block(&mut self) -> BlkId{
-        self.blocks.push(Block::new());
+        self.blocks.push(RefCell::new(Block::new()));
         return self.blocks.len() as BlkId
+    }
+
+    pub fn get_block<'b>(&'b self, id : BlkId) -> Ref<'b, Block<'a>> {
+        self.blocks[id as usize].borrow()
+    }
+
+    pub fn get_block_mut<'b>(&'b self, id : BlkId) -> RefMut<'b, Block<'a>> {
+        self.blocks[id as usize].borrow_mut()
     }
 }

eb7997a17c31ccbcbaeb79c51a124e1c1087ea64
Joachim-barre <joachim.barre22@laposte.net>
2024-08-25 12:05:01 +0200

    fixed block inplementation

--- shared/src/ir/block.rs
+++ shared/src/ir/block.rs
@@ -1,20 +1,19 @@
+use std::collections::LinkedList;
 use super::Instruction;
 
 pub struct Block<'a> {
-    pub instructions : Vec<Instruction<'a>>
+    pub instructions : LinkedList<Instruction<'a>>
 }
 
 impl<'a> Block<'a> {
     pub fn new() -> Self {
         Self {
-            instructions : Vec::new()
+            instructions : LinkedList::new()
         }
     }
 
-    pub fn push(&mut self, i : Instruction<'a>) -> &'a Instruction<'a> {
-        self.instructions.push(i);
-        unsafe {
-            std::mem::transmute(&self.instructions[self.instructions.len() - 1])
-        }
+    pub fn push<'b>(&'b mut self, i : Instruction<'a>) -> &'b Instruction<'a> {
+        self.instructions.push_back(i);
+        self.instructions.back().unwrap()
     }
 }

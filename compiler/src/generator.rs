dc7fe0a7e793cb483dbf9beb928316aae3331ddd
Joachim-barre <joachim.barre22@laposte.net>
2024-08-25 22:28:27 +0200

    fixed genrator

--- compiler/src/generator.rs
+++ compiler/src/generator.rs
@@ -26,11 +26,11 @@ impl IrGenerator {
     }
 
     pub fn get_current_block<'b>(&'b self) -> Option<Ref<'b, Block>> {
-        self.current_block.as_ref().map(|id| self.object.get_block(id))
+        self.current_block.as_ref().map(|id| self.object.get_block(*id))
     }
 
     pub fn get_current_block_mut<'b>(&'b self) -> Option<RefMut<'b, Block>> {
-        self.current_block.as_ref().map(|id| self.object.get_block_mut(id))
+        self.current_block.as_ref().map(|id| self.object.get_block_mut(*id))
     }
 
     pub fn add(&mut self, i : Instruction) -> Option<ListIndex> {

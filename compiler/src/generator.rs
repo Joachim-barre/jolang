d67823b0d7dda1754e7ac6d2252a7e67e5e6ba55
Joachim-barre <joachim.barre22@laposte.net>
2024-08-25 12:42:27 +0200

    added new for Ir Generator

--- compiler/src/generator.rs
+++ compiler/src/generator.rs
@@ -2,5 +2,14 @@ use jolang_shared::ir::{instructions::operand::BlkId, IrObject};
 
 struct IrGenerator<'a> {
     object : IrObject<'a>,
-    current_block : BlkId
+    current_block : Option<BlkId>
+}
+
+impl<'a> IrGenerator<'a> {
+    pub fn new() -> Self {
+        Self {
+            object : IrObject::new(),
+            current_block : None
+        }
+    }
 }

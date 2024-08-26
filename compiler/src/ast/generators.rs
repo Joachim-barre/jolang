e88427660f269f69ab4755eb5b9fd3c526f5a140
Joachim-barre <joachim.barre22@laposte.net>
2024-08-26 18:26:40 +0200

    made the genator use the new scope construction

--- compiler/src/ast/generators.rs
+++ compiler/src/ast/generators.rs
@@ -10,7 +10,7 @@ impl Generate for Program {
         generator.goto_begin(exit_block);
         let code = generator.add(Instruction::Iconst(0)).unwrap();
         generator.add(Instruction::Reti(code));
-        let scope = Scope::new(ScopeKind::Root, Some(exit_block));
+        let scope = Scope::new(ScopeKind::Root, blk, exit_block);
         generator.enter_scope(scope);
         generator.goto_begin(blk);
         for s in &self.0 {
@@ -25,11 +25,16 @@ impl Generate for Statement {
     fn generate(&self, generator : &mut IrGenerator) {
         match self {
             Self::Block(stmts) => {
-                let scope = Scope::new(ScopeKind::Block, None);
+                let block = generator.append_block();
+                generator.add(Instruction::Br(block));
+                generator.goto_begin(block);
+                let after_block = generator.append_block();
+                let scope = Scope::new(ScopeKind::Block, block, after_block);
                 generator.enter_scope(scope);
                 for s in stmts {
                     s.generate(generator);
                 }
+                generator.add(Instruction::Br(after_block));
                 generator.exit_scope();
             },
             Self::If(expr, then, _else) => {
@@ -46,7 +51,7 @@ impl Generate for Statement {
                 match **then {
                     Self::Block(_) => then.generate(generator),
                     _ => {
-                        let scope = Scope::new(ScopeKind::Block, Some(after_block));
+                        let scope = Scope::new(ScopeKind::Block, then_block, after_block);
                         generator.enter_scope(scope);
                         then.generate(generator);
                         generator.exit_scope();
@@ -58,7 +63,7 @@ impl Generate for Statement {
                     match **_else {
                         Self::Block(_) => then.generate(generator),
                         _ => {
-                            let scope = Scope::new(ScopeKind::Block, Some(after_block));
+                            let scope = Scope::new(ScopeKind::Block, else_block, after_block);
                             generator.enter_scope(scope);
                             _else.generate(generator);
                             generator.exit_scope();
@@ -73,11 +78,11 @@ impl Generate for Statement {
                 generator.add(Instruction::Br(while_cond));
                 let while_body = generator.append_block();
                 let after_block = generator.append_block();
-                let scope = Scope::new(ScopeKind::Loop, Some(after_block));
+                let scope = Scope::new(ScopeKind::Loop, while_cond, after_block);
                 generator.enter_scope(scope);
                 expr.generate(generator);
                 let cond = generator.get_current_block().unwrap().last_index();
-                generator.add(Instruction::Briz(after_block, while_body, cond));
+generator.add(Instruction::Briz(after_block, while_body, cond));
                 generator.goto_begin(while_body);
                 body.generate(generator);
                 generator.add(Instruction::Br(while_cond));
@@ -87,7 +92,7 @@ impl Generate for Statement {
             Self::Loop(body) => {
                 let loop_body = generator.append_block();
                 let after_block = generator.append_block();
-                let scope = Scope::new(ScopeKind::Loop, Some(after_block));
+                let scope = Scope::new(ScopeKind::Loop, loop_body, after_block);
                 generator.enter_scope(scope);
                 generator.goto_begin(loop_body);
                 body.generate(generator);

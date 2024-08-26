90c74446b1ecfd72880ae915fb6af303782b0d50
Joachim-barre <joachim.barre22@laposte.net>
2024-08-26 11:12:39 +0200

    fixed program generator

--- compiler/src/ast/generators.rs
+++ compiler/src/ast/generators.rs
@@ -1,10 +1,12 @@
 use crate::generator::{Generate, IrGenerator};
-use super::Program;
+use super::{Program, Statement};
 
 impl Generate for Program {
     fn generate(&mut self, generator : &mut IrGenerator) {
+        let blk = generator.append_block();
+        generator.goto_begin(blk);
         for s in &mut self.0 {
-            s.generate();
+            s.generate(generator);
         }
     }
 }

10772ed58b8d86384d7c669ae1955b0653f94d51
Joachim-barre <joachim.barre22@laposte.net>
2024-08-27 11:38:03 +0200

    updated instructions

--- shared/src/ir/instructions.rs
+++ shared/src/ir/instructions.rs
@@ -4,93 +4,84 @@ c_enum! {
     #[derive(PartialEq, Eq, Clone, Copy)]
     pub enum Opcodes : u8{ 
         Ret = 0x00,
-        Reti = 0x10,
-        Varget = 0x11,
-        Iconst = 0x12,
-        Br = 0x13,
-        Pusharg = 0x14,
-        Call = 0x15,
-        Neg = 0x16,
-        Varset = 0x20,
-        Add = 0x21,
-        Sub = 0x22,
-        Mul = 0x23,
-        Div = 0x24,
-        Eq = 0x25,
-        Ne = 0x26,
-        Gt = 0x27,
-        Ge = 0x28,
-        Le = 0x29,
-        Lt = 0x2a,
-        Lsh = 0x2b,
-        Rsh = 0x2c,
-        Briz = 0x30
+        Reti = 0x01,
+        Iconst = 0x02,
+        Br = 0x03,
+        Dup = 0x04,
+        Dupx = 0x05,
+        Call = 0x05,
+        Neg = 0x06,
+        Add = 0x07,
+        Sub = 0x08,
+        Mul = 0x09,
+        Div = 0x0a,
+        Eq = 0x0b,
+        Ne = 0x0c,
+        Gt = 0x0d,
+        Ge = 0x0e,
+        Le = 0x0f,
+        Lt = 0x10,
+        Lsh = 0x11,
+        Rsh = 0x12,
+        Briz = 0x13
     }
 }
 
 pub mod operand {
-    use index_list::ListIndex;
-
-    use super::Instruction;
-
     pub type Imm = i64;
-    pub type VarId = u64;
     pub type BlkId = u64;
-    pub type Result = ListIndex;
     pub type FnId = u64;
 }
 
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum Instruction {
     Ret(),
-    Reti(operand::Result),
-    Varget(operand::VarId),
+    Reti(),
     Iconst(operand::Imm),
     Br(operand::BlkId),
-    Pusharg(operand::Result),
+    Dup(),
+    Dupx(operand::Imm),
     Call(operand::FnId),
-    Neg(operand::Result),
-    Varset(operand::VarId,operand::Result),
-    Add(operand::Result,operand::Result),
-    Sub(operand::Result,operand::Result),
-    Mul(operand::Result,operand::Result),
-    Div(operand::Result,operand::Result),
-    Eq(operand::Result,operand::Result),
-    Ne(operand::Result,operand::Result),
-    Gt(operand::Result,operand::Result),
-    Ge(operand::Result,operand::Result),
-    Le(operand::Result,operand::Result),
-    Lt(operand::Result,operand::Result),
-    Lsh(operand::Result,operand::Result),
-    Rsh(operand::Result,operand::Result),
-    Briz(operand::BlkId,operand::BlkId,operand::Result)
+    Neg(),
+    Add(),
+    Sub(),
+    Mul(),
+    Div(),
+    Eq(),
+    Ne(),
+    Gt(),
+    Ge(),
+    Le(),
+    Lt(),
+    Lsh(),
+    Rsh(),
+    Briz(operand::BlkId, operand::BlkId)
 }
 
 impl Instruction {
     pub fn opcode(&self) -> Opcodes {
         match self {
             Self::Ret() => Opcodes::Ret,
-            Self::Reti(_) => Opcodes::Reti,
-            Self::Varget(_) => Opcodes::Varget,
+            Self::Reti() => Opcodes::Reti,
             Self::Iconst(_) => Opcodes::Iconst,
             Self::Br(_) => Opcodes::Br,
-            Self::Pusharg(_) => Opcodes::Pusharg,
+            Self::Dup() => Opcodes::Dup,
+            Self::Dupx(_) => Opcodes::Dupx,
             Self::Call(_) => Opcodes::Call,
-            Self::Neg(_) => Opcodes::Neg,
-            Self::Varset(_, _) => Opcodes::Varset,
-            Self::Add(_, _) => Opcodes::Add,
-            Self::Sub(_, _) => Opcodes::Sub,
-            Self::Mul(_, _) => Opcodes::Mul,
-            Self::Div(_, _) => Opcodes::Div,
-            Self::Eq(_, _) => Opcodes::Eq,
-            Self::Ne(_, _) => Opcodes::Ne,
-            Self::Gt(_, _) => Opcodes::Gt,
-            Self::Ge(_, _) => Opcodes::Ge,
-            Self::Le(_, _) => Opcodes::Le,
-            Self::Lt(_, _) => Opcodes::Lt,
-            Self::Lsh(_, _) => Opcodes::Lsh,
-            Self::Rsh(_, _) => Opcodes::Rsh,
-            Self::Briz(_, _, _) => Opcodes::Briz
+            Self::Neg() => Opcodes::Neg,
+            Self::Add() => Opcodes::Add,
+            Self::Sub() => Opcodes::Sub,
+            Self::Mul() => Opcodes::Mul,
+            Self::Div() => Opcodes::Div,
+            Self::Eq() => Opcodes::Eq,
+            Self::Ne() => Opcodes::Ne,
+            Self::Gt() => Opcodes::Gt,
+            Self::Ge() => Opcodes::Ge,
+            Self::Le() => Opcodes::Le,
+            Self::Lt() => Opcodes::Lt,
+            Self::Lsh() => Opcodes::Lsh,
+            Self::Rsh() => Opcodes::Rsh,
+            Self::Briz(_, _) => Opcodes::Briz,
         }
     }
 }

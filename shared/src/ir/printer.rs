8ea8a3593a3bf6a321c50c6230b1d1070295bc87
Joachim-barre <joachim.barre22@laposte.net>
2024-08-27 16:20:10 +0200

    finiched coding the printer

--- shared/src/ir/printer.rs
+++ shared/src/ir/printer.rs
@@ -1,6 +1,6 @@
 use core::fmt;
 use std::fmt::Debug;
-
+use super::instructions::Instruction;
 use super::IrObject;
 
 pub fn write_ir(format : &mut std::fmt::Formatter, ir : &IrObject) -> fmt::Result {
@@ -14,9 +14,30 @@ pub fn write_ir(format : &mut std::fmt::Formatter, ir : &IrObject) -> fmt::Resul
         for i in b.1.instructions.iter() {
             write!(format, "\t")?;
             match i {
-                super::instructions::Instruction::Ret() => write!(format, "ret"),
-                _ => todo!()
+                Instruction::Ret() => write!(format, "ret"),
+                Instruction::Reti() => write!(format, "reti"),
+                Instruction::Iconst(v) => write!(format, "iconst {}", v),
+                Instruction::Br(b) => write!(format, "br B{}", b),
+                Instruction::Dup() => write!(format, "dup"),
+                Instruction::Dupx(v) => write!(format, "dupx {}", v),
+                Instruction::Swap() => write!(format, "swap"),
+                Instruction::Call(id) => write!(format, "call {}", id),
+                Instruction::Neg() => write!(format, "neg"),
+                Instruction::Add() => write!(format, "add"),
+                Instruction::Sub() => write!(format, "sub"),
+                Instruction::Mul() => write!(format, "mul"),
+                Instruction::Div() => write!(format, "div"),
+                Instruction::Eq() => write!(format, "eq"),
+                Instruction::Ne() => write!(format, "ne"),
+                Instruction::Gt() => write!(format, "gt"),
+                Instruction::Ge() => write!(format, "ge"),
+                Instruction::Le() => write!(format, "le"),
+                Instruction::Lt() => write!(format, "lt"),
+                Instruction::Lsh() => write!(format, "lsh"),
+                Instruction::Rsh() => write!(format, "rsh"),
+                Instruction::Briz(b1, b2) => write!(format, "briz {} {}", b1, b2)
             }?;
+            write!(format, "\n")?;
         }
     }
     write!(format, "}}")?;

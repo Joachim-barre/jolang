677b198337ec94032fac8bc656d48c5c2937ce4b
Joachim-barre <joachim.barre22@laposte.net>
2024-08-27 11:54:23 +0200

    added stack size to blocks

--- shared/src/ir/block.rs
+++ shared/src/ir/block.rs
@@ -3,14 +3,17 @@ use super::instructions::Instruction;
 #[derive(Debug)]
 pub struct Block {
     pub argc : u8,
-    pub instructions : Vec<Instruction>
+    pub instructions : Vec<Instruction>,
+    // size of the stack at the end of the block (after the last instruction)
+    pub stack_size : u64
 }
 
 impl Block {
     pub fn new(argc : u8) -> Self {
         Self {
             argc,
-            instructions : Vec::new()
+            instructions : Vec::new(),
+            stack_size : argc as u64
         }
     }
 }

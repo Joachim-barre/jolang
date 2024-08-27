4a112814cb8d82ee74e9c565144c2d5525b7c02a
Joachim-barre <joachim.barre22@laposte.net>
2024-08-27 20:07:01 +0200

    temporarly made the runtime print the ir

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,6 +1,13 @@
-use std::path::PathBuf;
+use std::{fs::OpenOptions, path::PathBuf};
 use anyhow::Result;
+use jolang_shared::ir::reader::read;
 
 pub fn run(file : PathBuf) -> Result<()> {
-    todo!() 
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(false)
+        .create(false)
+        .open(file)?;
+    dbg!(read(&mut file)?);
+    todo!()
 }

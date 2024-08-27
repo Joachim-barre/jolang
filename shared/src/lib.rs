881016a0911c738f9ac0bc6ec2be73eff35ff20c
Joachim-barre <joachim.barre22@laposte.net>
2024-08-27 19:14:38 +0200

    added version static

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,2 +1,10 @@
+use semver::Version;
+
 pub mod ffi;
 pub mod ir;
+
+pub static VERSION_STR : &str = env!("CARGO_PKG_VERSION");
+
+lazy_static::lazy_static!{
+    pub static ref VERSION : Version = Version::parse(VERSION_STR).expect("cannot parse package version");
+}

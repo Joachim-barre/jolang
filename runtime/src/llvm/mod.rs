7e15d5cd1e87c617117e1cd77f7dc89569abd534
Joachim-barre <joachim.barre22@laposte.net>
2024-08-29 19:08:27 +0200

    made ther runtime implement the trait

--- runtime/src/llvm/mod.rs
+++ runtime/src/llvm/mod.rs
@@ -1,12 +1,13 @@
 use inkwell::{context::Context, module::Module, builder::Builder};
+use crate::Runtime;
 
 pub struct LLVMRuntime<'ctx> {
     ctx : Context,
     module : Option<Module<'ctx>>,
 }
 
-impl<'a> LLVMRuntime<'a> {
-    pub fn new() -> Self {
+impl<'a> Runtime for LLVMRuntime<'a> {
+    fn new() -> Self {
         Self {
             ctx : Context::create(),
             module : None

92c32dc26745a8ad18a09c42e57f9dc1b4044835
Joachim-barre <joachim.barre22@laposte.net>
2024-08-29 19:12:16 +0200

    implemented run

--- runtime/src/llvm/mod.rs
+++ runtime/src/llvm/mod.rs
@@ -1,4 +1,4 @@
-use inkwell::{context::Context, module::Module, builder::Builder};
+use inkwell::{context::Context, module::Module, builder::Builder, OptimizationLevel, execution_engine::JitFunction};
 use crate::Runtime;
 
 pub struct LLVMRuntime<'ctx> {
@@ -11,6 +11,20 @@ impl<'a> Runtime for LLVMRuntime<'a> {
         Self {
             ctx : Context::create(),
             module : None
+        }
+    }
+
+    fn run(&mut self) -> i64 {
+        if self.module.is_none() {
+            panic!("tried to run an unitialized module");
+        }
+        let module = self.module.as_mut().unwrap();
+        let execution_engine = module.create_jit_execution_engine(OptimizationLevel::Default).unwrap();
+
+        unsafe {
+            type MainFn = unsafe extern "C" fn() -> i64;
+            let main_fn: JitFunction<MainFn> = execution_engine.get_function("main").unwrap();
+            main_fn.call()
         }
     }
 }

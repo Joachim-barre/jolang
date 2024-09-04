3d982ea4076e461e56c5adf3c8e4c365ad8a64ee
Joachim-barre <joachim.barre22@laposte.net>
2024-09-04 14:18:40 +0200

    added colon to tokens

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -30,6 +30,7 @@ pub enum TokenKind {
     LParan,
     RParan,
     Semicolon,
+    Colon,
     Equal,
     DoubleEqual,
     NotEqual,
@@ -174,6 +175,7 @@ impl<'a> Iterator for Lexer<'a> {
                 '(' => Some(TokenKind::LParan),
                 ')' => Some(TokenKind::RParan),
                 ';' => Some(TokenKind::Semicolon),
+                ':' => Some(TokenKind::Colon),
                 '+' => Some(TokenKind::Plus),
                 '*' => Some(TokenKind::Times),
                 '/' => Some(TokenKind::Divider),
@@ -242,14 +244,15 @@ mod tests {
     fn test_single_char() {
         let buf = SourceBuffer {
             path : PathBuf::from("test.jol"),
-            buffer : String::from("{}();+*/-,=><")
+            buffer : String::from("{}();:+*/-,=><")
         };
         let tokens = vec![
             TokenKind::LCurly, 
             TokenKind::RCurly, 
             TokenKind::LParan, 
             TokenKind::RParan, 
-            TokenKind::Semicolon, 
+            TokenKind::Semicolon,
+            TokenKind::Colon,
             TokenKind::Plus, 
             TokenKind::Times, 
             TokenKind::Divider, 
